4e18b59d8202dc1996cad4b591cd62b89cacc0e1
Lidia <lidava@gmail.com>
2024-01-18 09:35:04 +0400

    Added 2 cols.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -6,11 +6,20 @@
             class="border rounded-md mb-3 border-blue-600 p-3"
             v-for="row in props.resumeRows"
         >
-            <div class="font-bold px-3 py-5">{{ moment( row.date_start , "YYYY-MM-DD HH:mm:ss").format('MM.YYYY') }}-{{ moment( row.date_end , "YYYY-MM-DD HH:mm:ss").format('MM.YYYY') }}</div>
-            <div class="font-bold py-5 px-3">{{ row.company?.name }}</div>
-            <div class="font-bold py-5 px-3">{{ row.job_title }}</div>
-            <div class="px-3">
-                <div v-html="row.job_description"></div>
+            <div class="grid grid-cols-12">
+                <div class="col-start-1 col-end-2">
+                    <div class="font-bold px-3 py-5">{{
+                            moment(row.date_start, "YYYY-MM-DD HH:mm:ss").format('MM.YYYY')
+                        }}-{{ moment(row.date_end, "YYYY-MM-DD HH:mm:ss").format('MM.YYYY') }}
+                    </div>
+                </div>
+                <div class="col-start-3 col-end-12">
+                    <div class="font-bold py-5 px-3">{{ row.company?.name }}</div>
+                    <div class="font-bold py-5 px-3">{{ row.job_title }}</div>
+                    <div class="px-3">
+                        <div v-html="row.job_description"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </GuestLayout>
@@ -20,6 +29,7 @@
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import {Head} from "@inertiajs/vue3";
 import moment from 'moment';
+
 moment.locale("ru") //установка русской локали
 
 
